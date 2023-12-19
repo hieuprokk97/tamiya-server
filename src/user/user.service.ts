58e1de2ff3e7c35dcf6d50f758c192b946112186
thinhtp <tranphuocthinh2001@gmail.com>
2023-12-19 11:53:59 +0700

    .

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -22,6 +22,7 @@ export class UserService {
       .createQueryBuilder()
       .where('LOWER(UNACCENT(name)) = :name', { name: normalizedUsername })
       .getExists();
+    console.log('🚀 ~ existUser:', existUser);
 
     if (existUser) {
       throw new ExceptionResponse(HttpStatus.BAD_REQUEST, 'Tên người dùng đã tồn tại');
